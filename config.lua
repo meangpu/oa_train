4ce9518ce385c09f08190223c9ccf1902ab975ee
meangpu <meangpu@gmail.com>
2026-06-09 21:11:36 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ Config.Location = {
     ["ARMADILO"] = {
         npcLocation = vector4(-3729.075, -2601.290, -12.888, 172.615)
     },
-    ["Rhodes"] = {
+    ["RHODES"] = {
         npcLocation = vector4(1230.248, -1298.516, 76.954, 221.872)
     },
 }
