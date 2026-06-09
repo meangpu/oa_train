e6a12e7aed957c810f0fa49eead0c2e1538fec29
meangpu <meangpu@gmail.com>
2026-06-09 21:11:31 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,5 +3,10 @@ Config.DebugPrintClient = true
 Config.DebugPrintServer = true
 
 Config.Location = {
-    ["Armadilo"] = vector4(-3729.075, -2601.290, -12.888, 172.615)
+    ["ARMADILO"] = {
+        npcLocation = vector4(-3729.075, -2601.290, -12.888, 172.615)
+    },
+    ["Rhodes"] = {
+        npcLocation = vector4(1230.248, -1298.516, 76.954, 221.872)
+    },
 }
