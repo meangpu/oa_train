a4a6f17ff323187e3e0b7a862075c18df53303e1
meangpu <meangpu@gmail.com>
2026-06-09 22:08:49 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,8 @@ Config = {}
 Config.DebugPrintClient = true
 Config.DebugPrintServer = true
 
+local minute = 60 * 1000
+
 Config.Location = {
     ["ARMADILO"] = {
         npcLocation = vector4(-3729.075, -2601.290, -12.888, 172.615)
@@ -13,4 +15,11 @@ Config.Location = {
         npcLocation = vector4(2674.400, -1459.525, 46.341, 110.163)
         -- อันนี้ยังไม่เสร็จ
     },
+}
+
+Config.WaitTime = {
+    default = 60 * minute,
+    vip_small = 45 * minute,
+    vip_medium = 30 * minute,
+    vip_large = 15 * minute,
 }
