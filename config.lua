1338203f091578be12dd44e8f993cc2ecbcbefa4
meangpu <meangpu@gmail.com>
2026-06-10 10:01:50 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,14 +3,12 @@ local minute = 60 * 1000
 
 Config.Location = {
     ["ARMADILO"] = {
-        npcLocation = vector4(-3729.075, -2601.290, -12.888, 172.615)
+        npcLocation = vector4(-3729.075, -2601.290, -12.888, 172.615),
+        exitLocation = vector4(0, 0, 0, 0)
     },
     ["RHODES"] = {
-        npcLocation = vector4(1230.248, -1298.516, 76.954, 221.872)
-    },
-    ["SAINT"] = {
-        npcLocation = vector4(2674.400, -1459.525, 46.341, 110.163)
-        -- อันนี้ยังไม่เสร็จ
+        npcLocation = vector4(1230.248, -1298.516, 76.954, 221.872),
+        exitLocation = vector4(0, 0, 0, 0)
     },
 }
 
